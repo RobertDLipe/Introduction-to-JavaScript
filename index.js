8eaceb7842f3efdb28e8c965f69b937df32c0f93
Robert Lipe <liper2000gmail.com>
2021-02-28 14:42:52 -0500

    I cannot figure out the bottle or the RPS tasks, I will need to get assistance with those.

--- index.js
+++ index.js
@@ -203,10 +203,10 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(songstart){
+  for (let songstart <= 5; songstart < numbers; songstart--)
 
+console.log (annoyingSong ((i) bottles of soda on the wall, (i) bottles of soda, take one down pass it around, (i-1) bottles of soda on the wall))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
